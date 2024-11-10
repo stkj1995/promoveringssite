97f9433eed8e66119f619b54f49997e0d1dbe9e2
Sophie <soph1155@stud.kea.dk>
2024-11-10 17:13:07 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        pastelgreyblue: "#C3D1E3", // Pastel gråblå
         turkish: "#51A1E1", //turkis
         kingblue: "#0E4E87", // Kongeblå
         white: "#ffffff", // Hvid
-        black: "#000000", //Sort
+        almostblack: "#03101C", //Sort
         beige: "#F7F5EF", //lys beige
       },
     },
