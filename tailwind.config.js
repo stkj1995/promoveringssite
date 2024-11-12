98fd7f9339322e98cdfc5ae44294882c8f4fa41a
Sophie <soph1155@stud.kea.dk>
2024-11-12 12:11:28 +0100

    font styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      
       colors: {
         turkish: "#3D9AEB", //turkis
         kingblue: "#0E4E87", // Kongeblå
@@ -11,6 +12,23 @@ export default {
         beige: "#F7F5EF", //lys beige
         pastelblue: "#DFEEFC", //pastelblå
         brown: "#817141", //brun
+      },
+      fontFamily: {
+        playfair: ['Playfair Display', 'serif'], // Generel font-family for Playfair
+      },
+      fontWeight: {
+        light: '300',   // Light vægt
+        regular: '400', // Normal vægt
+        bold: '700',    // Bold vægt
+        medium: '500',  // Medium vægt
+        semiBold: '600', // Semi-Bold vægt
+        extraBold: '800', // Extra-Bold vægt
+      },
+      fontSize: {
+        h1: '48px',  // H1 størrelse
+        h2: '40px',  // H2 størrelse
+        h3: '32px',  // H3 størrelse
+        p: '16px',   // P (paragraf) størrelse
       },
       animation: {
         'fade-zoom': 'fadeZoom 1s ease-in-out',
